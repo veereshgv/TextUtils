5d59ec1acfd4662e15acc883570b29c2e05141db
VEEERSH G V <bkveeresh@gmail.com>
2021-12-19 12:33:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
 
             <Route
               exact
-              path="/TextUtils/"
+              path="/"
               element={
                 <TextForm
                   showAlert={showAlert}
